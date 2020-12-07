3043681eab20f5e08f428a7503ce5a708cb731dc
nghia nguyen <futuregohan1997@gmail.com>
2020-12-07 10:39:12 +0700

    make it better

--- index.py
+++ index.py
@@ -1,15 +1,24 @@
   
-from high_order_framework_requests_python import utils_class
-# from utils_db import *
 from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
 
-# db_connection_str = 'mysql+pymysql://mysql_user:mysql_password@mysql_host/mysql_db'
+# B1: loc trung
+
+ip_vps='45.77.36.114'
+db_name='auto_spam_ib_cho_tot'
+username='nghiahsgs4'
+password='261997'
+
+db_connection_str = 'mysql+pymysql://%s:%s@%s/%s'%(username,password,ip_vps,db_name)
+
 db_connection = create_engine(db_connection_str)
 
-df = pd.read_sql('SELECT * FROM product', con=db_connection)
+df = pd.read_sql('SELECT * FROM spam_user', con=db_connection)
 
 
-df=df.drop_duplicates(subset=['href'])
-df.to_csv('b.csv')
+df=df.drop_duplicates(subset=['link_chat'])
+# df.to_csv('b.csv')
+
+
+# B2: import nguoc lai db
